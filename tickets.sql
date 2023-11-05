6cbe2e834b9e66d06aadfb7bfbe34a882e4468e2
Rokas Paulauskas <ifamous.rp@gmail.com>
2023-11-05 17:43:43 +0100

    SQL Script for database

---
+++ tickets.sql
@@ -0,0 +1,83 @@
+-- mysql
+-- Create database `Tickets`
+CREATE DATABASE `Tickets` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE `Tickets`;
+CREATE TABLE `language` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Language` varchar(255) NOT NULL,
+  `code` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `language_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_email` (`email`),
+  FOREIGN KEY (`language_id`) REFERENCES `language`(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `badge` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `requirement` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `mockExam` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `percentage` varchar(255) NOT NULL,
+    `user_id` int(11) NOT NULL,
+    `completion_date` datetime NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `topic` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `name` varchar(255) NOT NULL,
+    `description` varchar(255) NOT NULL,
+    `image` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `question` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `question` varchar(255) NOT NULL,
+    `topic_id` int(11) NOT NULL,
+    `image` varchar(255) NOT NULL,
+    `explanation` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`topic_id`) REFERENCES `topic`(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `answer` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `answer` varchar(255) NOT NULL,
+    `question_id` int(11) NOT NULL,
+    `is_correct` int(11) NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`question_id`) REFERENCES `question`(`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `userBadge` (
+    `user_id` int(11) NOT NULL,
+    `badge_id` int(11) NOT NULL,
+    PRIMARY KEY (`user_id`, `badge_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`),
+    FOREIGN KEY (`badge_id`) REFERENCES `badge`(`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `questionStatus` (
+    `user_id` int(11) NOT NULL,
+    `question_id` int(11) NOT NULL,
+    `status` int(11) NOT NULL,
+    PRIMARY KEY (`user_id`, `question_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`),
+    FOREIGN KEY (`question_id`) REFERENCES `question`(`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `subscription` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `user_id` int(11) NOT NULL,
+    `subscription_start_date` datetime NOT NULL,
+    `subscription_end_date` datetime NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
+    ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
